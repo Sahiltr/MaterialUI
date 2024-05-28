02dd55157b201641123f43454fb9e54d07323348
Sahil Thakur <thakursahil1912@gmail.com>
2024-05-28 23:13:08 +0530

    basics Covered

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
-import { Button, Typography} from "@mui/material";
+import { Button, Typography, styled} from "@mui/material";
 import SettingsIcon from '@mui/icons-material/Settings'; 
 import { Add } from "@mui/icons-material";
 function App() {
+
+  const BlueButton = styled(Button)({
+    backgroundColor:"skyblue",
+    color:"#888",
+    margin:5,
+    "&:hover":{
+      backgroundColor:"lightblue"
+    },
+    "&:disabled":{
+      backgroundColor:"gray",
+      color:"white"
+    }
+  })
+
+  
   return (
     <div className="App">
       <Button variant="text">Text</Button>
@@ -10,18 +25,8 @@ function App() {
       <Button variant="outlined">Outlined</Button>
 
       <Typography variant="h2" component="p">  It's a ptag using h2 </Typography>
-      <Button variant="contained" sx={{
-        backgroundColor:"skyblue",
-        color:"#888",
-        margin:5,
-        "&:hover":{
-          backgroundColor:"lightblue"
-        },
-        "&:disabled":{
-          backgroundColor:"gray",
-          color:"white"
-        }
-      }}>Add new post</Button>
+      <BlueButton>add</BlueButton>
+      <BlueButton>sub</BlueButton>
 
 
     </div>
